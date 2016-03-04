8aa3562074537b0d1d42fd74b414e1382944d36c
Patrick Shushereba <pshushereba@gmail.com>
2016-03-04 04:05:08 +0000

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Agileprep::Application.config.secret_key_base = 'f3f2552b2085048cf795952a49c910c4419b8dc8d5d30235abe37f96c1cea7d937b4e000198513ffa74db41ef95998dbd65d57d2984cf7f3776ec324bc5940aa'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Agileprep::Application.config.session_store :cookie_store, key: '_agileprep_session'
